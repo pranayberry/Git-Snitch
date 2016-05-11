8041c9263069b47d9abbe7c29ff74af6724e615c
Pranay Berry <pranayberry@gmail.com>
2016-05-11 22:41:56 +0530

    Contribution summary functionality added

--- script.py
+++ script.py
@@ -9,13 +9,34 @@ def scrape(url):
 		return soup
 	return None
 
+def fname(page):
+	return page.find('div','vcard-fullname').string
+
+def contribution(page):
+	i = 0
+	print "\nContribution Summary\n"
+	print "Date\t\t\tNo of Contributions"
+	while i<5:
+		for day in page.find_all('rect'):
+			properties = day.attrs
+			date = datetime.today() - timedelta(i)
+			date = date.strftime("%Y-%m-%d")
+			if properties['data-date'] == date:
+				print date,"\t\t\t",
+				print properties['data-count']
+		i += 1
+
+
 def display(url):
 	page = scrape(url)
 	if page == None:
 		print "Invalid Username"
 	else:
-		print "scrape successful"
-
+		fullName = fname(page)
+		#print "\n******************************************************"
+		print "\n",fullName
+		print "@%s" % username
+		contribution(page)
 
 
 username = raw_input("Enter github username\n> ")
